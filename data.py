c90d5d281b44da97553f015fae8d54ddba28a9de
orkhanchel <149297384+orkhanchel@users.noreply.github.com>
2023-11-07 16:49:00 +0400

    Update data.py

--- data.py
+++ data.py
@@ -1,29 +1,29 @@
-#kortedj = ('abc', 'dawa')
-#print(s[1][0])
+kortedj = ('abc', 'dawa')
+print(s[1][0])
 
-#list = ['pawa', 'sawa', 222]
-#print(lis[1][0])
-#lis.append('nawa')
-#del lis[3]
+list = ['pawa', 'sawa', 222]
+print(lis[1][0])
+lis.append('nawa')
+del lis[3]
 
-#x = [s, list]
-#print(x)
-# print(x[1][0][0])
-# print(type(x[0]))
-#y = s, list
-#print(y)
-#print(type(list[1]))
+x = [s, list]
+print(x)
+print(x[1][0][0])
+print(type(x[0]))
+y = s, list
+print(y)
+print(type(list[1]))
 
-# dict = {'ключ' : 'значение', 'яблоко' : 'яблоне', 'комната' : 'квартира'}
-# dict['комната'] = 'дом'
-# print(dict)
-# dict['имя'] = 'человек'
-# print(dict)
-# del dict['имя']
-# print(dict)
+dict = {'ключ' : 'значение', 'яблоко' : 'яблоне', 'комната' : 'квартира'}
+dict['комната'] = 'дом'
+print(dict)
+dict['имя'] = 'человек'
+print(dict)
+del dict['имя']
+print(dict)
 
 
-# array = [22, 33, 44, 'hi']
-# for i in range(len(array)):
-#     print(array[i])
+array = [22, 33, 44, 'hi']
+for i in range(len(array)):
+    print(array[i])
 'не нужно ничего менять'
